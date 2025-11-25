70af7c85850b97173903ab612ee7a05ae33b4777
mschleie <mschleie@students.uni-mainz.de>
2025-11-25 13:43:48 +0100

    added solutions to 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+// weather codes as json file from https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
+import codes from './descriptions.json'
+
 const Search = (props) => {
   return (
     <p>
@@ -23,7 +26,7 @@ const Result = (props) => {
     console.log("Details")
     // detailed information if only one country is found
     return (
-      <Country country={props.filteredCountries[0]}/>
+      <Country country={props.filteredCountries[0]} weather={props.weather}/>
     )
   } else {
     // show filtered countries
@@ -39,7 +42,6 @@ const CountryList = (props) => {
 
   return (
     <div>
-      <h1>HERE</h1>
       <ul>
         {props.countryList.map(country => <li key={country.ccn3}>{country.name.common} <button onClick={() => props.handleShowDetails(country.ccn3)}>show</button></li>)}
       </ul>
@@ -64,15 +66,46 @@ const Country = (props) => {
       <div>
         <img src={props.country.flags["png"]}></img>
       </div>
+      <Weather capital={props.country.capital[0]} weather={props.weather}/>
     </div>
   )
 }
+
+const Weather = (props) => {
+  console.log("Weather", props.weather)
+  // do not render weather component if api response is not yet available
+  if (!props.weather) {
+    return (
+      <div></div>
+    )
+  }
+  // get image of weather code (WMO Code)
+  console.log("Codes", codes)
+  const imageUrl = codes[props.weather.current.weather_code]["day"]["image"]
+  console.log(props.weather.current.weather_code)
+  console.log(imageUrl)
+
+  // if weather data is availabe, render weather of capital
+  return (
+    <div>
+      <h2>Weather in {props.capital}</h2>
+      <p>Temperature {props.weather.current.temperature_2m} {props.weather.current_units.temperature_2m}</p>
+      <img src={imageUrl}></img>
+      <p>Wind {props.weather.current.wind_speed_10m} {props.weather.current_units.wind_speed_10m}</p>
+    </div>
+  )
+}
+/*
+      <p>Temperature {props.weather.current.temperature_2m}°</p>
+      <p>Wind {props.weather.current.wind_speed_10m}</p>
+*/
 
 const App = () => {
 
   const [findValue, setFindValue] = useState("")
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [weather, setWeather] = useState(null)
 
   // initial api call -> get all countries
   useEffect(() => {
@@ -81,6 +114,21 @@ const App = () => {
     .get("https://studies.cs.helsinki.fi/restcountries/api/all")
     .then(response => setCountries(response.data))
   }, [])
+
+  // get weather data only if filteredCountries contains 1 element
+  useEffect(() => {
+    if (filteredCountries.length === 1) {
+      // get data from https://open-meteo.com/ without API Key necessary
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${filteredCountries[0].latlng[0]}&longitude=${filteredCountries[0].latlng[1]}&current=temperature_2m,wind_speed_10m,weather_code`
+      
+      axios
+        .get(url)
+        .then(response => setWeather(response.data))
+        .catch(error => console.log(error))
+    } else {
+      setWeather(null)
+    }
+  }, [filteredCountries])
 
   const handleFindChange = (event) => {
     // update find input
@@ -99,7 +147,7 @@ const App = () => {
   return (
     <div>
       <Search findValue={findValue} handleFindChange={handleFindChange}/>
-      <Result filteredCountries={filteredCountries} handleShowDetails={handleShowDetailsOf}/>
+      <Result filteredCountries={filteredCountries} handleShowDetails={handleShowDetailsOf} weather={weather}/>
     </div>
   )
 }
