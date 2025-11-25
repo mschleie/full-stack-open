8e8a217892dbecc9267bb88eaeed84f605987bd1
mschleie <mschleie@students.uni-mainz.de>
2025-11-25 12:33:38 +0100

    added solution for 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -14,34 +14,34 @@ const Result = (props) => {
   console.log("Filtered Length", props.filteredCountries.length)
 
   if (props.filteredCountries.length > 10) {
+    console.log("Too many")
     // too many results
     return (
       <p>Too many matches, speficy another filter</p>
     )
   } else if (props.filteredCountries.length == 1) {
+    console.log("Details")
     // detailed information if only one country is found
     return (
       <Country country={props.filteredCountries[0]}/>
     )
   } else {
     // show filtered countries
+    console.log("Countrylist")
     return (
-      <CountryList countryList={props.filteredCountries}/>
+      <CountryList countryList={props.filteredCountries} handleShowDetails={props.handleShowDetails}/>
     )
   }
 }
 
 const CountryList = (props) => {
+  console.log(props.countryList)
+
   return (
     <div>
+      <h1>HERE</h1>
       <ul>
-        {props.countryList.map(country => {
-          <li key={country.ccn3}>
-            {country.name.common}
-            <button onClick={props.handleShow}>show</button>
-          </li>
-          })
-        }
+        {props.countryList.map(country => <li key={country.ccn3}>{country.name.common} <button onClick={() => props.handleShowDetails(country.ccn3)}>show</button></li>)}
       </ul>
     </div>
   )
@@ -72,6 +72,7 @@ const App = () => {
 
   const [findValue, setFindValue] = useState("")
   const [countries, setCountries] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
 
   // initial api call -> get all countries
   useEffect(() => {
@@ -82,23 +83,23 @@ const App = () => {
   }, [])
 
   const handleFindChange = (event) => {
-    setFindValue(event.target.value)
+    // update find input
+    console.log(event.target.value)
+    const val = event.target.value
+    setFindValue(val)
+    // also filter countries
+    const filtered = val === "" ? countries : countries.filter(country => country.name.common.toLowerCase().includes(val.toLowerCase()))
+    setFilteredCountries(filtered)
   }
 
-  const filterCountries = () => {
-    const res = findValue !== "" ? countries.filter(country => country.name.common.toLowerCase().includes(findValue.toLowerCase())) : countries
-    // console.log("FILTER", res)
-    return res
-  }
-
-  const handleShowDetailsOf = (id) => {
-    
+  const handleShowDetailsOf = (ccn3) => {
+    setFilteredCountries(countries.filter(country => country.ccn3 === ccn3))
   }
 
   return (
     <div>
       <Search findValue={findValue} handleFindChange={handleFindChange}/>
-      <Result filteredCountries={filterCountries()}/>
+      <Result filteredCountries={filteredCountries} handleShowDetails={handleShowDetailsOf}/>
     </div>
   )
 }
